a5a46fa9383c64efafcd3d6cf7a288e358b12c16
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-05 15:55:08 +0200

    added ; to js - added aestethic with bootstrap

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -4,14 +4,16 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito22 */
 
-alert("Ciao ! Creiamo insieme una password insicura !")
+alert("Ciao ! Creiamo insieme una password insicura !");
 
-let userName = prompt("Qual'é il tuo nome ?")
-let userSurname = prompt("Qual'é il tuo cognome ?")
-let userFavoriteColor = prompt("Qual'é il tuo colore preferito ?")
+let userName = prompt("Qual'é il tuo nome ?");
+let userSurname = prompt("Qual'é il tuo cognome ?");
+let userFavoriteColor = prompt("Qual'é il tuo colore preferito ?");
 
 
-const currentYear = ("2022")
-const testo = ("Ed ecco a te un\'insicurissima password:")
+const currentYear = ("2022");
+const testo = ("Ed ecco a te un\'insicurissima password:");
 
-document.getElementById("unreliable_pswrd").innerHTML = (`${testo} ${userName}${userSurname}${userFavoriteColor}${currentYear}`)
+document.getElementById("testo").innerHTML = (`${testo}`);
+
+document.getElementById("unreliable_pswrd").innerHTML = (`${userName}${userSurname}${userFavoriteColor}${currentYear}`);
