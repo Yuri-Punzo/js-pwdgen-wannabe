d9ad6e8e036fc17ff3f8d46147a170070c97b005
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-05 13:50:37 +0200

    initial commit

---
+++ assets/JS/main.js
@@ -0,0 +1,18 @@
+/* Istruzioni
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito22 */
+
+
+
+
+
+let userName = prompt("Qual'é il tuo nome ?")
+let userSurname = prompt("Qual'é il tuo cognome ?")
+let userFavoriteColor = prompt("Qual'é il tuo colore preferito ?")
+
+
+const currentYear = ("2022")
+
+.getElementById("unreliable_pswrd").innerHTML = "userName"
